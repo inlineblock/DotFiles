bec11165376a77d5c53c38c32c6afbf18a140b52
Dennis Wilkins <dennis.wilkins@moneydesktop.com>
2012-03-09 10:21:35 -0700

    Install script updates to be questions

--- install.sh
+++ install.sh
@@ -10,6 +10,56 @@ removeFiles()
 	rm -f ~/.gitconfig ~/.gitignore_global ~/.gvimrc ~/.inputrc ~/.jshintrc ~/.jsl.conf ~/.vimrc ~/.vundles ~/.zshrc
 }
 
+makeLinks()
+{
+	mkdir -p ~/.vim/bundle
+
+	ln -s $DIR/.gitignore_global ~/
+	ln -s $DIR/.gvimrc ~/
+	ln -s $DIR/.inputrc ~/
+	ln -s $DIR/.jshintrc ~/
+	ln -s $DIR/.jsl.conf ~/
+	ln -s $DIR/.vimrc ~/
+	ln -s $DIR/.vundles ~/
+	ln -s $DIR/.zshrc ~/
+
+	cp $DIR/.gitconfig ~/.gitconfig
+}
+
+vundlize ()
+{
+	if [ -d ~/.vim/bundle/vundle ]; then
+		cd ~/.vim/bundle/vundle
+		git pull origin master
+	else
+		git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+	fi
+	vim +BundleInstall +qall
+
+	# now we need to build command T
+	cd ~/.vim/bundle/Command-T/ruby/command-t/
+	rvm use system
+	CC=/usr/bin/gcc ruby extconf.rb
+	CC=/usr/bin/gcc make
+}
+
+setgit ()
+{
+	echo ""
+	echo ""
+	echo "Git commit name: "
+	read gname
+	if [[ ! -z $gname ]] ; then
+		git config --global user.name "$gname"
+	fi
+	echo ""
+	echo "Git commit email: "
+	read gemail
+	if [[ ! -z $gemail ]] ; then
+		git config --global user.email "$gemail"
+	fi
+}
+
 while true; do
 	read -p "Do you want to remove existing files? " yn
 	case $yn in
@@ -19,39 +69,32 @@ while true; do
 	esac
 done
 
+while true; do
+	read -p "Do you want to make copy the config and make symlinks? " yn
+	case $yn in
+		[Yy]* ) makeLinks; break;;
+		[Nn]* ) break;;
+		* ) echo "Please answer yes or no.";;
+	esac
+done
 
-mkdir -p ~/.vim/bundle
+while true; do
+	read -p "Do you want install Vundle and plugins? " yn
+	case $yn in
+		[Yy]* ) vundlize; break;;
+		[Nn]* ) break;;
+		* ) echo "Please answer yes or no.";;
+	esac
+done
 
-cp $DIR/.gitconfig ~/
-ln -s $DIR/.gitignore_global ~/
-ln -s $DIR/.gvimrc ~/
-ln -s $DIR/.inputrc ~/
-ln -s $DIR/.jshintrc ~/
-ln -s $DIR/.jsl.conf ~/
-ln -s $DIR/.vimrc ~/
-ln -s $DIR/.vundles ~/
-ln -s $DIR/.zshrc ~/
 
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +BundleInstall +qall
+while true; do
+	read -p "Do you want setup your gitconfig credentials? " yn
+	case $yn in
+		[Yy]* ) setgit; break;;
+		[Nn]* ) break;;
+		* ) echo "Please answer yes or no.";;
+	esac
+done
 
-# now we need to build command T
-cd ~/.vim/bundle/Command-T/ruby/command-t/
-rvm use system
-CC=/usr/bin/gcc ruby extconf.rb
-CC=/usr/bin/gcc make
-
-echo ""
-echo ""
-echo "Git commit name: "
-read gname
-if [[ ! -z $gname ]] ; then
-	git config --global user.name "$gname"
-fi
-echo ""
-echo "Git commit email: "
-read gemail
-if [[ ! -z $gemail ]] ; then
-	git config --global user.email "$gemail"
-fi
 
